bebd2f887b02d8f4dff89484f97ea936dfa1d97f
David.Liu <david.liu@dingtone.me>
2021-01-13 16:12:45 +0800

    学习

---
+++ LuaLearn/src/数据类型与运算符/002_Learn1.lua
@@ -0,0 +1,5 @@
+str2 = nil
+numArray = {12,3,5,66,88,99}
+
+print(type(str2))
+print(type(numArray))
---
+++ LuaLearn/src/数据类型与运算符/003_lua编辑IDE.lua
@@ -0,0 +1,17 @@
+--[[
+
+使用IDEA的lua插件
+	lua网站下载内置编辑IDE(SciIDE)最大的缺点是，在使用过程中，不能有效区分英文与中文，故在大量编写商业项目中，比较繁琐。且没有“代码不全”与“智能提示”功能。
+	其他lua编辑器介绍：
+		1、LuaDist（官方推荐）http://luadist/org/
+		2、LuaStudio国内开发优秀IDE http://www.luastudio.net
+		3、VS lua 插件（babelua插件）http://www.archive.codeplex.com/?p=babelua
+		4、IEDA
+IDEA安装与lua插件配置
+
+]]--
+--[[
+IDEA安装与lua插件配置
+	IDEA全称IntelliJ IDEA,是java语言开发的集成环境，IntelliJ在业界被公认为最好的java开发工具之一。尤其在智能代码助手、代码自动提示、重构J2EE支持
+	各类版本工具（git\svn\github等）、Junit、CVS整合、代码分析、创新的GUI设计等方面的功能可以说是超常的。
+]]--
