bc450ead254c64a8feabd03dfaa6c54074cde1ba
David.Liu <david.liu@dingtone.me>
2021-03-19 19:40:04 +0800

    学习模块

--- LuaLearn/src/中级篇/C008_函数进阶下/036_模块的概念与定义.lua
+++ LuaLearn/src/中级篇/C008_函数进阶下/036_模块的概念与定义.lua
@@ -7,6 +7,24 @@
 模块
     模块的定义：
         由变量、函数等组成的table,因此创建模块本质就是创建一个table,此table最后需要返回。
-
-
+    模块的作用：
+        类似于一个封装库，把一些公用的代码放在一个文件里，以API接口的形式在其他地方调用，有利于代码的重用和降低代码耦合度。
+    注意事项：
+        定义local的函数名，就不要加模块限定，否则会报错。
 ]]
+--定义一个局部模块
+local myModel = {}
+--定义一模块中的字段
+myModel.gHeight = 180
+function myModel.Fun1()
+    print("Fun1 method invoked")
+end
+function myModel.FUn2()
+    print("Fun2 method invoked")
+end
+
+-- 模块中不能定义私有的（local）函数与“字段”，否则无法调用。
+--local myModel.Fun3()
+--    print("Fun3 method invoked")
+--end
+return myModel
