5550adc9e2fb96e2b5afc8927c2bb61bc7989227
David.Liu <david.liu@dingtone.me>
2021-04-21 19:13:25 +0800

    协同程序学习

--- LuaLearn/src/中级篇/C013_协同程序/065_协同_协同的参数传递.lua
+++ LuaLearn/src/中级篇/C013_协同程序/065_协同_协同的参数传递.lua
@@ -9,8 +9,18 @@
         1 > 定义带有形参的函数，且定义为协同程序。
         2 > 使用coroutine.resume(协同，“协同参数”）方式传入协同的实参。
 ]]
-cor1 = coroutine.create(
-        function()
+--演示一个参数的传递
+function fun1(num1)
+        print("出入参数num1  =",num1)
+end
+cor1 = coroutine.create(fun1)
+--测试运行
+coroutine.resume(cor1,200) --200 就是协同的”实参“
+print("演示多个参数的传递")
+-- cor 是一个"thread"
+function fun2(cor,...)
+        print("协同内部，输入参数协同的状态",coroutine.status(cor),...)
+end
 
-        end
-)
+cor2 = coroutine.create(fun2)
+coroutine.resume(cor2,cor2,"hello",123,"世界")
